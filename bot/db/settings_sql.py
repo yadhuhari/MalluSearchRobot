fa0ef4032b0826832ad7e1d1e53811e50cb99277
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2025-04-06 14:33:18 +0530

    Update settings_sql.py

--- bot/db/settings_sql.py
+++ bot/db/settings_sql.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm.exc import NoResultFound
-from mfinder import DB_URL, LOGGER
+from bot import DB_URL, LOGGER
 
 
 BASE = declarative_base()
